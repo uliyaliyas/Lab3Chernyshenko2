0c12a2e100c44d0c9cc06404a9592f3da8e8f4eb
5090969 <5090969@LAPTOP-NB62C61V>
2023-11-06 23:30:03 +0200

    Добавьте файлы проекта.

---
+++ Lab3Chernyshenko2/Program.cs
@@ -0,0 +1,17 @@
+﻿int m; 
+Console.Write("Введите число m: ");
+m = int.Parse(Console.ReadLine());
+int number = 5;
+int result = 0;
+do
+{
+    int square = number * number;
+    if (number % 100 == 0)
+    {
+        int divisionResult = square / m;
+        Console.WriteLine($"({number}^2) / {m} = {divisionResult}");
+    }
+
+    number += 5;
+} 
+while (number <= 1000);
